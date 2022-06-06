44f61789bd62ea2027533cf57c9d5793c188961e
Scott Foster <scott.foster@data61.csiro.au>
2022-06-06 18:12:31 +1000

    adding bias term predictions

--- R/plot.isdm1.R
+++ R/plot.isdm1.R
@@ -12,7 +12,16 @@ plot.isdm <- function( object, covarRaster, ...){
   rasCount <- raster::rasterize( POspP, covarRaster, fun='count', background=0)
   rasCount <- raster::mask( rasCount, covarRaster[[1]])
   
-  resids <- (values( rasCount) - preds$mean.field$mu.mean) / preds$mean.field$mu.mean
+  resids <- stats::ppois( raster::values( rasCount), raster::values( preds$mean.field$mu.mean))
+  tmp <- stats::ppois( pmax( raster::values( rasCount)-1, 0), raster::values( preds$mean.field$mu.mean))
+  tmp[values( rasCount)==0] <- 0
+  tmp1 <- stats::runif( n=length( resids), min=resids, max=tmp)
+  
+  
+  
+  
+  
+  resids <- (values( rasCount) - preds$mean.field$mu.mean) / sqrt(preds$mean.field$mu.mean)
   resids <- raster::rasterFromXYZ( cbind( resids, raster::coordinates( rasCount)))
   
   
