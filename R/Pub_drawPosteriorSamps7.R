24b6358a34f1b1bbec53966263a58ccef03a60e1
Scott Foster <scott.foster@data61.csiro.au>
2022-11-28 13:52:29 +1100

    temporary change to predict.isdm for debugging an analysis

--- R/Pub_drawPosteriorSamps7.R
+++ R/Pub_drawPosteriorSamps7.R
@@ -28,8 +28,8 @@ draw.posterior.samps <- function(inla.fm, B=100, what="params", field="iSpat",n.
     colnames( draws) <- c( rownames( inla.fm$summary.fixed), "Spatial.Variance", "kappa", "Smooth")
   
     message( "Using INLA's posterior.sample for random effects")
-    message( class( inla.fm))
-    message( names( inla.fm))
+    message( "class", class( inla.fm))
+    message( "names", names( inla.fm))
     tmp <- INLA::inla.posterior.sample( n=B, result=inla.fm, intern=FALSE, add.names=FALSE)
     message( "finished with INLA's posterior.sample")
     latent.ids.wanted <- utils::tail( rownames( tmp[[1]]$latent), p.fixed)
