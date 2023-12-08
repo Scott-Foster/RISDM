9400e2be9ab7985fa191f73180a8649387a2d733
Scott Foster <scott.foster@data61.csiro.au>
2023-12-08 17:08:14 +1100

    first doodlings in updating the DC code for a single prob-of-detection param

---
+++ R/prepareSingleSurvey_commonPi.R
@@ -0,0 +1,70 @@
+
+###############################################################################################
+###############################################################################################
+####	
+####	For a single DC survey, arrange expansion stuff, pre-estimate
+####
+####	Returns a matrix for each survey full of good bits 'n' pieces for DC data estimation 
+####
+####	Programmed by Scott in the first half of 2022
+####
+###############################################################################################
+###############################################################################################
+
+prepareSingleSurvey <- function( singleDataSet, datasetID, DCcols, survIDname, sampAreaDC, DCmethod){
+  
+  n <- nrow( singleDataSet)
+  
+  #the bits and pieces for approximating log( 1-pi)
+  expansPis <- estimatePisDoubleCount_commonPi( singleDataSet[,DCcols])
+  singleDataSet$Obs1_pi <- expansPis[1]
+  singleDataSet$Obs2_pi <- expansPis[2]
+  
+  #reformatting and filling in
+  county <- unlist( singleDataSet[,DCcols])
+  index <- rep( 1:nrow( singleDataSet), times=3)  #once for each double count type
+  singleDataSet <- singleDataSet[index,]
+  singleDataSet$DCcountDC <- county #this variable name shouldn't get confused with others...?
+
+  #identifiers
+  singleDataSet$observer <- rep( DCcols, each=n)
+  
+  #removing unwanted cols
+  singleDataSet <- singleDataSet[,-(1:3)]
+  
+  #storing the original offset before it is altered.
+  singleDataSet$originalSampleArea <- singleDataSet[,sampAreaDC]
+  
+  #the offset for plugin observer prob.
+  if( DCmethod=="plugin"){
+    offy <- rep( NA, 3*n)
+    offy[1:n] <- expansPis[1]*(1-expansPis[2])
+    offy[n+1:n] <- (1-expansPis[1]) * expansPis[2]
+    offy[2*n + 1:n] <- expansPis[1] * expansPis[2]
+    singleDataSet$observerOffy <- offy
+    singleDataSet[,sampAreaDC] <- singleDataSet[,sampAreaDC] * offy
+    
+    return( singleDataSet)
+  }
+
+  #the offset for TaylorLinApprox of log( 1-pi) around log( pi)  
+  singleDataSet$expansOffset <- singleDataSet$alpha1Coef <- singleDataSet$alpha2Coef <- NA
+  #Obs1 (not Obs2)
+  singleDataSet$expansOffset[1:n] <- log( 1-expansPis[2]) - (expansPis[2] / (expansPis[2]-1)) * log( expansPis[2])
+  singleDataSet$alpha1Coef[1:n] <- 1
+  singleDataSet$alpha2Coef[1:n] <- expansPis[2] / (expansPis[2]-1)
+  #Obs2 (not Obs1)
+  singleDataSet$expansOffset[n+1:n] <- log( 1-expansPis[1]) - (expansPis[1] / (expansPis[1]-1)) * log( expansPis[1])
+  singleDataSet$alpha1Coef[n+1:n] <- expansPis[1] / (expansPis[1]-1)
+  singleDataSet$alpha2Coef[n+1:n] <- 1
+  #Both
+  singleDataSet$expansOffset[2*n+1:n] <- 0
+  singleDataSet$alpha1Coef[2*n+1:n] <- 1
+  singleDataSet$alpha2Coef[2*n+1:n] <- 1
+
+  singleDataSet[,sampAreaDC] <- exp( log( singleDataSet[,sampAreaDC]) + singleDataSet$expansOffset)
+    
+  return( singleDataSet)
+  
+}
+
