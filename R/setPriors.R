73fc20a3f402e6aa03ce7525ac6a594012de20b1
Scott Foster <scott.foster@data61.csiro.au>
2024-05-28 11:56:33 +1000

    More bug fixes to prior specification and updated help for the same

--- R/setPriors.R
+++ R/setPriors.R
@@ -14,27 +14,35 @@
 setPriors <- function( control, stck){
 
   if( !is.null( control$prior.list)){
-    if( ! names( control$prior.list) %in% c("prec","mean"))
+    if( ! any( names( control$prior.list) %in% c("prec","mean")))
       stop( "Priors for fixed effects contain elements that are not 'mean' nor 'prec'. Please check (see ?isdm too)")
-    if( !is.null(control$prior.list$mean) & !names( control$prior.list$mean) %in% stck$effects$names)
+    tmpNames <- names( control$prior.list$mean)
+    tmpNames <- setdiff( tmpNames, "default")
+    if( !is.null(control$prior.list$mean) & !all( tmpNames %in% stck$effects$names))
       message( "Priors set for the mean of at least one fixed effect that is not in the formula.  Please check.")
-    if( !is.null(control$prior.list$prec) & !names( control$prior.list$prec) %in% stck$effects$names)
+    tmpNames <- names( control$prior.list$prec)
+    tmpNames <- setdiff( tmpNames, "default")
+    if( !is.null(control$prior.list$prec) & !all( tmpNames %in% stck$effects$names))
       message( "Priors set for the precision of at least one fixed effect that is not in the formula. Please check.")
-    return( control$prior.list)
+#    return( control$prior.list)
+    tmp <- control$prior.list
+    if( !"default" %in% names( tmp$mean))
+      tmp$mean$default <- control$prior.mean
+    if( !"default" %in% names( tmp$prec))
+      tmp$prec$default <- control$other.prec #intercepts will be updated separately and presently
   }
+  else
+    tmp <- list( mean=list( default=control$prior.mean), prec=list( default=control$other.prec)) #intercepts will be updated separately and presently
 
-  #container for the result
-  tmp <- list( mean=control$prior.mean)
-  tmp$prec <- list( default=control$other.prec)
-  #for the intercept terms, in turn of data type.
-  if( "PO_Intercept" %in% colnames( stck$effects$data))
+  if( ("PO_Intercept" %in% stck$effects$names) & (!"PO_Intercept" %in% names( tmp$prec)))
     tmp$prec$PO_Intercept <- control$int.prec
-  if( "PA_Intercept" %in% colnames( stck$effects$data))
+  if( ("PA_Intercept" %in% stck$effects$names) & (!"PA_Intercept" %in% names( tmp$prec)))
     tmp$prec$PA_Intercept <- control$int.prec
-  if( "AA_Intercept" %in% colnames( stck$effects$data))
+  if( ("AA_Intercept" %in% stck$effects$names) & (!"AA_Intercept" %in% names( tmp$prec)))
     tmp$prec$AA_Intercept <- control$int.prec
-  if( "DC_Intercept" %in% colnames( stck$effects$data))
+  if( ("DC_Intercept" %in% stck$effects$names) & (!"DC_Intercept" %in% names( tmp$prec)))
     tmp$prec$DC_Intercept <- control$int.prec
+    
   return( tmp)
 }
 
